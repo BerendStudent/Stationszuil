3e31b16067933da0aee25c7d2a6c585bd5ddd86e
BerendStudent <144696095+BerendStudent@users.noreply.github.com>
2023-09-20 12:58:53 +0200

    Begin FA

---
+++ Final_Assignment_Berend_Broesder.py
@@ -0,0 +1,8 @@
+print("""1: Ik wil weten hoeveel kluizen nog vrij zijn
+2: Ik wil een nieuwe kluis
+3: Ik wil even iets uit mijn kluis halen
+4: Ik geef mijn kluis terug""")
+
+choice = input("Kies een optie:\n")
+
+print("Je hebt gekozen:", input)
