06033e523d7e773d00a56c639bd6d4d8bf326adf
BerendStudent <144696095+BerendStudent@users.noreply.github.com>
2023-11-07 03:02:58 +0100

    SQL stap 3 en 4, werkt principieel, moet nog kunnen stoppen.

--- Statuinszuil moderator.py
+++ Statuinszuil moderator.py
@@ -7,28 +7,38 @@ email = input('Voer uw email adres in: ')
 naam = input('Voer uw naam in: ')
 connection = psycopg2.connect(
     database="Project Stationszuil",
-    user="Cursor",
-    password="Password",
+    user="postgres",
+    password="password",
     host="localhost",
     port="5432"
 )
 cursor = connection.cursor()
-
-with open('data.csv', 'r') as file:
-    with open('reviews.csv', 'a') as newFile:
-        reader = csv.reader(file)
-        writer = csv.writer(newFile)
-        for row in reader:
-            print(str(row))
-            goed = input('Goedgekeurd, Y/N: ')
-            if goed == 'Y':
-                row.append(naam)
-                row.append(email)
-                writer.writerow(row)
-                print("Goedgekeurd.")
-            elif goed == 'N':
-                print("Afgekeurd.")
-            else:
-                print('Y of N')
-        os.remove('data.csv')
-        print('Alles is nagekeken.')
+while True:
+    with open('data.csv', 'r') as file:
+        with open('reviews.csv', 'a', newline='') as newFile:
+            reader = csv.reader(file)
+            writer = csv.writer(newFile)
+            print('Voer STOP in om te stoppen.')
+            for row in reader:
+                print(str(row))
+                goed = input('Goedgekeurd, Y/N: ')
+                if goed == 'Y':
+                    row.append(naam)
+                    row.append(email)
+                    writer.writerow(row)
+                    query = """insert into Admins (Adminnaam, email) VALUES (%s, %s) ON CONFLICT DO NOTHING; 
+                    insert into berichten (naam, datum, stationsnaam, inhoud, Email) 
+                    VALUES (%s,%s,%s,%s,%s);"""
+                    invoer = (naam, email, row[0], row[1], row[2], row[3], email)
+                    cursor.execute(query, invoer)
+                    connection.commit()
+                    print("Goedgekeurd.")
+                elif goed == 'N':
+                    writer.writerow(row)
+                    print("Afgekeurd.")
+                elif goed == 'STOP':
+                    break
+                else:
+                    print('Y of N')
+            os.remove('data.csv')
+            print('Alles is nagekeken.')
